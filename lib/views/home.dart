cdaee2c23fb263d4fd4d80bf6879cc8b8e5e3657
brayanbertan <brayanbertan@gmail.com>
2021-03-01 21:02:20 -0300

    SingleChildScrollView

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -22,20 +22,82 @@ class _HomePageState extends State<HomePage> {
         ),
         centerTitle: true,
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                margin: EdgeInsets.all(5.0),
-                width: MediaQuery.of(context).size.width * 0.2,
-                height: MediaQuery.of(context).size.height * 0.5,
-                child: Observer(builder: (_) {
-                  return ListView(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  margin: EdgeInsets.all(5.0),
+                  width: MediaQuery.of(context).size.width * 0.2,
+                  height: MediaQuery.of(context).size.height * 0.5,
+                  child: Observer(builder: (_) {
+                    return ListView(
+                        padding: EdgeInsets.zero,
+                        children: controller.listaCorretos.map((copo) {
+                          return Container(
+                            alignment: Alignment.bottomCenter,
+                            padding: EdgeInsets.zero,
+                            margin: EdgeInsets.only(bottom: 5.0),
+                            width: 80.0,
+                            height: 120.0,
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                border: Border(
+                                  bottom: BorderSide(
+                                      color: Colors.greenAccent, width: 3.0),
+                                  right: BorderSide(
+                                      color: Colors.greenAccent, width: 3.0),
+                                  left: BorderSide(
+                                      color: Colors.greenAccent, width: 3.0),
+                                )),
+                            child: Container(
+                              margin: EdgeInsets.zero,
+                              width: 80.0,
+                              height: 100.0,
+                              color: color,
+                              child: Center(
+                                child: Text(
+                                  '${copo}',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            ),
+                          );
+                        }).toList());
+                  }),
+                ),
+                Expanded(
+                    child: Container(
+                  height: MediaQuery.of(context).size.height * 0.5,
+                  decoration: BoxDecoration(
+                      border: Border(
+                    bottom: BorderSide(color: Colors.black, width: 3.0),
+                    right: BorderSide(color: Colors.black, width: 3.0),
+                    left: BorderSide(color: Colors.black, width: 3.0),
+                  )),
+                  child: Container(
+                    margin: EdgeInsets.only(top: 50.0),
+                    color: color,
+                    child: Center(
+                      child: Observer(
+                          builder: (_) => Text(
+                                '${controller.galao}',
+                                style: TextStyle(color: Colors.white),
+                              )),
+                    ),
+                  ),
+                )),
+                Container(
+                  margin: EdgeInsets.all(5.0),
+                  width: MediaQuery.of(context).size.width * 0.2,
+                  height: MediaQuery.of(context).size.height * 0.5,
+                  child: Observer(builder: (_) {
+                    return ListView(
                       padding: EdgeInsets.zero,
-                      children: controller.listaCorretos.map((copo) {
+                      children: controller.listaIncorretos.map((copo) {
                         return Container(
                           alignment: Alignment.bottomCenter,
                           padding: EdgeInsets.zero,
@@ -46,11 +108,11 @@ class _HomePageState extends State<HomePage> {
                               color: Colors.white,
                               border: Border(
                                 bottom: BorderSide(
-                                    color: Colors.greenAccent, width: 3.0),
+                                    color: Colors.redAccent, width: 3.0),
                                 right: BorderSide(
-                                    color: Colors.greenAccent, width: 3.0),
+                                    color: Colors.redAccent, width: 3.0),
                                 left: BorderSide(
-                                    color: Colors.greenAccent, width: 3.0),
+                                    color: Colors.redAccent, width: 3.0),
                               )),
                           child: Container(
                             margin: EdgeInsets.zero,
@@ -59,151 +121,91 @@ class _HomePageState extends State<HomePage> {
                             color: color,
                             child: Center(
                               child: Text(
-                                '${copo}',
+                                '$copo',
                                 style: TextStyle(color: Colors.white),
                               ),
                             ),
                           ),
                         );
-                      }).toList());
-                }),
-              ),
-              Expanded(
-                  child: Container(
-                height: MediaQuery.of(context).size.height * 0.5,
-                decoration: BoxDecoration(
-                    border: Border(
-                  bottom: BorderSide(color: Colors.black, width: 3.0),
-                  right: BorderSide(color: Colors.black, width: 3.0),
-                  left: BorderSide(color: Colors.black, width: 3.0),
-                )),
-                child: Container(
-                  margin: EdgeInsets.only(top: 50.0),
-                  color: color,
-                  child: Center(
-                    child: Observer(
-                        builder: (_) => Text(
-                              '${controller.galao}',
-                              style: TextStyle(color: Colors.white),
-                            )),
+                      }).toList(),
+                    );
+                  }),
+                )
+              ],
+            ),
+            Container(
+              padding: EdgeInsets.all(5.0),
+              margin: EdgeInsets.all(15.0),
+              height: 120.0,
+              child: Observer(builder: (_) {
+                return ListView.separated(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: controller.listaTodos.length,
+                  separatorBuilder: (BuildContext context, int index) => SizedBox(
+                    width: 15.0,
                   ),
-                ),
-              )),
-              Container(
-                margin: EdgeInsets.all(5.0),
-                width: MediaQuery.of(context).size.width * 0.2,
-                height: MediaQuery.of(context).size.height * 0.5,
-                child: Observer(builder: (_) {
-                  return ListView(
-                    padding: EdgeInsets.zero,
-                    children: controller.listaIncorretos.map((copo) {
-                      return Container(
-                        alignment: Alignment.bottomCenter,
-                        padding: EdgeInsets.zero,
-                        margin: EdgeInsets.only(bottom: 5.0),
-                        width: 80.0,
-                        height: 120.0,
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            border: Border(
-                              bottom: BorderSide(
-                                  color: Colors.redAccent, width: 3.0),
-                              right: BorderSide(
-                                  color: Colors.redAccent, width: 3.0),
-                              left: BorderSide(
-                                  color: Colors.redAccent, width: 3.0),
-                            )),
-                        child: Container(
-                          margin: EdgeInsets.zero,
-                          width: 80.0,
-                          height: 100.0,
-                          color: color,
-                          child: Center(
-                            child: Text(
-                              '$copo',
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ),
-                        ),
-                      );
-                    }).toList(),
-                  );
-                }),
-              )
-            ],
-          ),
-          Container(
-            padding: EdgeInsets.all(5.0),
-            margin: EdgeInsets.all(15.0),
-            height: 120.0,
-            child: Observer(builder: (_) {
-              return ListView.separated(
-                scrollDirection: Axis.horizontal,
-                itemCount: controller.listaTodos.length,
-                separatorBuilder: (BuildContext context, int index) => SizedBox(
-                  width: 15.0,
-                ),
-                itemBuilder: (context, index) {
-                  return controller.listaTodos == null
-                      ? Container()
-                      : Container(
-                          alignment: Alignment.bottomCenter,
-                          padding: EdgeInsets.zero,
-                          margin: EdgeInsets.only(bottom: 5.0),
-                          width: 80.0,
-                          decoration: BoxDecoration(
-                              border: Border(
-                            bottom: BorderSide(
-                                color: Colors.yellowAccent, width: 3.0),
-                            right: BorderSide(
-                                color: Colors.yellowAccent, width: 3.0),
-                            left: BorderSide(
-                                color: Colors.yellowAccent, width: 3.0),
-                          )),
-                          child: Container(
-                            margin: EdgeInsets.zero,
+                  itemBuilder: (context, index) {
+                    return controller.listaTodos == null
+                        ? Container()
+                        : Container(
+                            alignment: Alignment.bottomCenter,
+                            padding: EdgeInsets.zero,
+                            margin: EdgeInsets.only(bottom: 5.0),
                             width: 80.0,
-                            height: 80.0,
-                            color: color,
-                            child: Center(
-                              child: Text(
-                                '${controller.listaTodos[index]}',
-                                style: TextStyle(color: Colors.white),
+                            decoration: BoxDecoration(
+                                border: Border(
+                              bottom: BorderSide(
+                                  color: Colors.yellowAccent, width: 3.0),
+                              right: BorderSide(
+                                  color: Colors.yellowAccent, width: 3.0),
+                              left: BorderSide(
+                                  color: Colors.yellowAccent, width: 3.0),
+                            )),
+                            child: Container(
+                              margin: EdgeInsets.zero,
+                              width: 80.0,
+                              height: 80.0,
+                              color: color,
+                              child: Center(
+                                child: Text(
+                                  '${controller.listaTodos[index]}',
+                                  style: TextStyle(color: Colors.white),
+                                ),
                               ),
                             ),
-                          ),
-                        );
-                },
-              );
-            }),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              buildLegenda(Colors.yellowAccent, 'Todos'),
-              buildLegenda(Colors.greenAccent, 'Resposta'),
-              buildLegenda(Colors.redAccent, 'Resto'),
-              Observer(builder: (_) {
-                return Text(
-                    'Sobra: ${(controller.galaoVolumeAtual).toStringAsFixed(2)}');
+                          );
+                  },
+                );
               }),
-            ],
-          ),
-          Container(
-            margin: EdgeInsets.all(15.0),
-            height: 50.0,
-            child: RaisedButton(
-              color: color,
-              onPressed: () {
-                controller.gerarTeste();
-              },
-              child: Text(
-                'Gerar teste',
-                style: TextStyle(color: Colors.white),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                buildLegenda(Colors.yellowAccent, 'Todos'),
+                buildLegenda(Colors.greenAccent, 'Resposta'),
+                buildLegenda(Colors.redAccent, 'Resto'),
+                Observer(builder: (_) {
+                  return Text(
+                      'Sobra: ${(controller.galaoVolumeAtual).toStringAsFixed(2)}');
+                }),
+              ],
+            ),
+            Container(
+              margin: EdgeInsets.all(15.0),
+              height: 50.0,
+              child: RaisedButton(
+                color: color,
+                onPressed: () {
+                  controller.gerarTeste();
+                },
+                child: Text(
+                  'Gerar teste',
+                  style: TextStyle(color: Colors.white),
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
